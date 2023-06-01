84c306dd3b04cdb3c23d85abd9cc2b7e67a95b79
Edwin Cardenas <eacardenase@unal.edu.co>
2023-05-31 20:26:35 -0500

    feat: add get started button and conditionally show it on last item info

--- OnboardingProject/Controller/OnboardingController.swift
+++ OnboardingProject/Controller/OnboardingController.swift
@@ -14,14 +14,32 @@ class OnboardingController: UIViewController {
 
     private var onboardingItems = [OnboardingItemInfo]()
     private var onboardingView = PaperOnboarding()
+    
+    private let getStartedButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        button.setTitle("Get Started", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        button.addTarget(self, action: #selector(dismissOnboarding), for: .touchUpInside)
+        
+        return button
+    }()
 
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        onboardingView.delegate = self
 
         configureUI()
         configureOnboardingDataSource()
+    }
+    
+    // only works if not inside navigation bar
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 }
 
@@ -31,8 +49,12 @@ extension OnboardingController {
     private func configureUI() {
 
         onboardingView.translatesAutoresizingMaskIntoConstraints = false
+        getStartedButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        getStartedButton.alpha = 0
 
         view.addSubview(onboardingView)
+        view.addSubview(getStartedButton)
 
         // onboardingView
         NSLayoutConstraint.activate([
@@ -41,22 +63,58 @@ extension OnboardingController {
             onboardingView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             onboardingView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+        
+        // getStartedButton
+        NSLayoutConstraint.activate([
+            getStartedButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            getStartedButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -128)
+        ])
     }
 
     private func configureOnboardingDataSource() {
-        let item1 = OnboardingItemInfo(informationImage:
+        let metricsItem = OnboardingItemInfo(informationImage:
                                        UIImage(named: "baseline_insert_chart_white_48pt")!.withRenderingMode(.alwaysOriginal),
-                                       title: "Metrics", description: "Some description in here",
+                                       title: K.Onboarding.MSG_METRICS, description: K.Onboarding.MSG_ONBOARDING_METRICS,
                                        pageIcon: UIImage(), color: .systemPurple, titleColor: .white,
                                        descriptionColor: .white, titleFont: UIFont.systemFont(ofSize: 24),
                                        descriptionFont: UIFont.systemFont(ofSize: 16))
 
-        onboardingItems.append(item1)
-        onboardingItems.append(item1)
-        onboardingItems.append(item1)
+        let dashboardItem = OnboardingItemInfo(informationImage:
+                                       UIImage(named: "baseline_dashboard_white_48pt")!.withRenderingMode(.alwaysOriginal),
+                                       title: K.Onboarding.MSG_DASHBOARD, description: K.Onboarding.MSG_ONBOARDING_DASHBOARD,
+                                       pageIcon: UIImage(), color: .systemBlue, titleColor: .white,
+                                       descriptionColor: .white, titleFont: UIFont.systemFont(ofSize: 24),
+                                       descriptionFont: UIFont.systemFont(ofSize: 16))
+
+        let notificationsItem = OnboardingItemInfo(informationImage:
+                                       UIImage(named: "baseline_notifications_active_white_48pt")!.withRenderingMode(.alwaysOriginal),
+                                       title: K.Onboarding.MSG_NOTIFICATIONS, description: K.Onboarding.MSG_ONBOARDING_NOTIFICATIONS,
+                                       pageIcon: UIImage(), color: .systemPink, titleColor: .white,
+                                       descriptionColor: .white, titleFont: UIFont.systemFont(ofSize: 24),
+                                       descriptionFont: UIFont.systemFont(ofSize: 16))
+
+        onboardingItems.append(metricsItem)
+        onboardingItems.append(dashboardItem)
+        onboardingItems.append(notificationsItem)
 
         onboardingView.dataSource = self
         onboardingView.reloadInputViews()
+    }
+    
+    private func animateGetStartedButton(_ shouldShow: Bool) {
+        let alpha: CGFloat = shouldShow ? 1 : 0
+        
+        UIView.animate(withDuration: 0.5) {
+            self.getStartedButton.alpha = alpha
+        }
+    }
+}
+
+// MARK: - Actions
+
+extension OnboardingController {
+    @objc private func dismissOnboarding(_ sender: UIButton) -> Void {
+        print("DEBUG: Handle dismiss onboarding")
     }
 }
 
@@ -69,5 +127,15 @@ extension OnboardingController: PaperOnboardingDataSource {
 
     func onboardingItem(at index: Int) -> OnboardingItemInfo {
         return onboardingItems[index]
+    }
+}
+
+// MARK: - PaperOnboardingDelegate
+
+extension OnboardingController: PaperOnboardingDelegate {
+    func onboardingDidTransitonToIndex(_ index: Int) {
+        let shouldShow = index == onboardingItems.count - 1 ? true : false
+        
+        animateGetStartedButton(shouldShow)
     }
 }
--- OnboardingProject/Utils/Constants.swift
+++ OnboardingProject/Utils/Constants.swift
@@ -11,4 +11,14 @@ struct K {
     struct FStore {
         static let COLLECTION_USERS = Firestore.firestore().collection("users")
     }
+    
+    struct Onboarding {
+        static let MSG_METRICS = "Metrics"
+        static let MSG_DASHBOARD = "Dashboard"
+        static let MSG_NOTIFICATIONS = "Get Notified"
+        
+        static let MSG_ONBOARDING_METRICS = "Extract valuable insights and come up with data driven product initiatives to help grow your business"
+        static let MSG_ONBOARDING_DASHBOARD = "Everything you need all in one place, available through our dashboard feature"
+        static let MSG_ONBOARDING_NOTIFICATIONS = "Get notified when important stuff is happening, so you don't miss out on the action"
+    }
 }
